fbda79fc39bb1e611930b8a7a84d9af25bf79719
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-04 15:56:06 +0600

    feat: add nuxt page comp and theme
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-	<div>
-		<h1>Nuxt App :)</h1>
-	</div>
+	<NuxtPage />
 </template>
+
+<script setup lang="ts">
+	import { useHead } from "nuxt/app";
+
+	useTheme();
+
+	useHead({
+		title: "Nuxt App",
+		// titleTemplate: (title) => `My App - ${title}`,
+		viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+		charset: "utf-8",
+		meta: [{ name: "description", content: "My amazing site." }],
+		link: [
+			//
+			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+			{ rel: "preconnect", href: "https://fonts.gstatic.com" },
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap",
+			},
+		],
+	});
+</script>
