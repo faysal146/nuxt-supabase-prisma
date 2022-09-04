21b67b8d4e1b065b514bb8aca8c4bcb16a53751c
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-04 15:44:37 +0600

    chore: add tailwind to nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-import { defineNuxtConfig } from 'nuxt';
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+	// typescript: {
+	// 	typeCheck: true,
+	// 	strict: true,
+	// },
+	modules: ["@nuxtjs/tailwindcss"],
+});
