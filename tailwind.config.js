d1acce66d248f4fea83841549d14c6a93a0b1616
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-04 15:45:49 +0600

    configure tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	theme: {
+		fontFamily: {
+			sans: ["Source Sans Pro", "sans-serif"],
+		},
+	},
+	daisyui: {
+		themes: ["light", "aqua", "night"],
+	},
+};
