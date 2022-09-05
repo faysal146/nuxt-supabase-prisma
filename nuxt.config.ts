65260076447ee8ebb0b81f88712c3343d09db163
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-06 00:30:01 +0600

    ci: setup supabase with env var
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,16 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	// typescript: {
-	// 	typeCheck: true,
-	// 	strict: true,
-	// },
-	modules: ["@nuxtjs/tailwindcss"],
+	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+	runtimeConfig: {
+		// The private keys which are only available within server-side
+		supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
+		// Keys within public, will be also exposed to the client-side
+		public: {
+			supabase: {
+				url: process.env.SUPABASE_URL,
+				key: process.env.SUPABASE_KEY,
+			},
+		},
+	},
 });
