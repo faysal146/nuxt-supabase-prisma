d4caacbd3d1f0dfa2e2d294dbaac6d4987057ebe
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-07 00:58:40 +0600

    feat: profile page (not finish yet)
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ pages/profile.vue
@@ -0,0 +1,10 @@
+<template>
+	<NuxtLayout name="with-header"> </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+	const supabase = useSupabaseClient();
+	const user = supabase.auth.user();
+	console.log(user);
+	// supabase.
+</script>
