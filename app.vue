0f275ab8963556da3ec923b3ab1493e8c4774c68
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-06 19:55:36 +0600

    style: remove all imports
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 	<Teleport to="body">
 		<template v-if="toast.toastList.value.length > 0">
 			<div class="toast toast-top toast-end z-10">
-				<Toast
+				<AlertToast
 					v-for="tst in toast.toastList.value"
 					:message="tst.message"
 					:type="tst.type"
@@ -12,16 +12,12 @@
 					:toastKey="tst.key"
 					@close-toast="toast.removeToast"
 				/>
-				<!-- @close-toast="toast.hideToast()" -->
 			</div>
 		</template>
 	</Teleport>
 </template>
 
 <script setup lang="ts">
-	import Toast from "~/components/Alert/Toast.vue";
-	import { useToast } from "~/composables/useToast";
-
 	useTheme();
 	useHead({
 		title: "Nuxt App",
@@ -29,15 +25,7 @@
 		viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 		charset: "utf-8",
 		meta: [{ name: "description", content: "My amazing site." }],
-		link: [
-			//
-			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-			{ rel: "preconnect", href: "https://fonts.gstatic.com" },
-			{
-				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap",
-			},
-		],
+		link: [],
 	});
 	const toast = useToast();
 	const supabase = useSupabaseClient();
