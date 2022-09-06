e62b2ddb3b3879be4e310261125ada4a81683c89
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-09-06 18:49:53 +0600

    refactor: use vueuse composable for dark or light mode
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -1,49 +1,19 @@
-import { createGlobalState, useLocalStorage, usePreferredDark } from "@vueuse/core";
-import { useState } from "#app";
+import { createGlobalState, useDark, useToggle } from "@vueuse/core";
 
-type ThemeName = "night" | "light" | "aqua";
-interface ITheme {
-	current: ThemeName;
-	themes: string[];
-}
-
-const initVal: ITheme = {
-	current: "light",
-	themes: ["light", "aqua", "night"],
+const themes = {
+	light: "synthwave",
+	dark: "night",
 };
 
 export const useTheme = createGlobalState(() => {
-	const isDark = usePreferredDark();
-	if (isDark.value) {
-		initVal.current = "night";
-	}
-
-	const theme = useState<ITheme>("theme", () => useLocalStorage("theme", initVal, { deep: true }));
-
-	function toggleTheme(name?: ThemeName) {
-		if (typeof name === "undefined") {
-			const themeV = theme.value;
-			let currentThemeIndex = themeV.themes.findIndex(val => val === themeV.current);
-			if (currentThemeIndex !== -1) {
-				if (currentThemeIndex + 1 > themeV.themes.length - 1) {
-					currentThemeIndex = 0;
-				} else {
-					currentThemeIndex++;
-				}
-				const newTheme = themeV.themes[currentThemeIndex] as ThemeName;
-				theme.value.current = newTheme;
-				document.querySelector("html").dataset.theme = newTheme;
-			}
-		} else {
-			if (name !== theme.value.current) {
-				theme.value.current = name;
-			}
-		}
-	}
-
-	onMounted(() => {
-		document.querySelector("html").dataset.theme = theme.value.current;
+	const isDark = useDark({
+		selector: "html",
+		attribute: "data-theme",
+		valueDark: themes.dark,
+		valueLight: themes.light,
 	});
+	const toggleTheme = useToggle(isDark);
+	const currentTheme = computed(() => (isDark ? themes.dark : themes.light));
 
-	return { theme: readonly(theme), toggleTheme };
+	return { theme: { currentTheme, isDark }, toggleTheme };
 });
